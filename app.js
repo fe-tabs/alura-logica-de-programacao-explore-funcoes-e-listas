891548167b35358cd37f78b9de566a28f0ebf3c1
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-25 19:11:56 -0300

    :sparkles: feat: alert if guess is less or greater than secret number

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ let guess = prompt("Escolha um número entre 1 e 10: ");
 if (guess == secretNumber) {
   alert(`Você acertou o número secreto: ${secretNumber}!`);
 } else {
-  alert(`Você errou! O número secreto não é ${guess}!`);
+  if (guess > secretNumber) {
+    alert(`Você errou! O número secreto é menor que ${guess}!`);
+  } else {
+    alert(`Você errou! O número secreto é maior que ${guess}!`);    
+  }
 }
