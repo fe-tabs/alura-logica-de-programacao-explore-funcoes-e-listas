70c41301c79e509e7cac705c2fe17befc787dd1b
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-25 19:33:22 -0300

    :sparkles: feat: allow guesses until guess is right

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 alert("Boas vindas ao Jogo do Número Secreto!");
 
+let guess;
+let guesses = 1;
 let secretNumber = 9;
 
-let guess = prompt("Escolha um número entre 1 e 10: ");
+while (guess != secretNumber) {
+  guess = prompt("Escolha um número entre 1 e 10: ");
 
-if (guess == secretNumber) {
-  alert(`Você acertou o número secreto: ${secretNumber}!`);
-} else {
-  if (guess > secretNumber) {
-    alert(`Você errou! O número secreto é menor que ${guess}!`);
+  if (guess == secretNumber) {
+    break;
   } else {
-    alert(`Você errou! O número secreto é maior que ${guess}!`);    
+    if (guess > secretNumber) {
+      alert(`Você errou! O número secreto é menor que ${guess}!`);
+    } else {
+      alert(`Você errou! O número secreto é maior que ${guess}!`);    
+    }
+
+    guesses++;
   }
 }
+
+alert(`
+  Você acertou o número secreto: 
+  ${secretNumber} em ${guesses} tentativa${(guesses > 1) ? 's' : ''}!
+`);
